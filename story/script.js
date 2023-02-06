03a147deb5241ce68eb0911e81d1554fbba6da6e
A-Charvin <anandcharvin.g@gmail.com>
2023-02-05 20:39:05 -0500

    Update script.js

--- story/script.js
+++ story/script.js
@@ -1,23 +1,42 @@
 require([
-    "esri/config",
-    "esri/Map",
-    "esri/WebMap",
-    "esri/views/MapView",
+  "esri/config",
+  "esri/Map",
+  "esri/views/MapView",
 
+  "esri/layers/FeatureLayer"
 
-   ], function (esriConfig, WebMap, MapView) {
+], function(esriConfig,Map, MapView, FeatureLayer) {
 
-     esriConfig.apiKey = "AAPK63dc6f1377f44fbb9b5ba0d22ce2078ege009eQiv7i9ZH9WXTW6kCVNwNfM-bpC3Dx0cneLrgIGAcct--dMWXnDaKkVcoC6";
-     const webmap = new WebMap({
-      portalItem: {
-        id: "b42d2263155e4022a4d866c1baaca810"
-      }
+   // esriConfig.apiKey = "AAPK63dc6f1377f44fbb9b5ba0d22ce2078ege009eQiv7i9ZH9WXTW6kCVNwNfM-bpC3Dx0cneLrgIGAcct--dMWXnDaKkVcoC6";
+    
+    const map = new Map({
+      basemap: "streets-vector"
     });
 
     const view = new MapView({
+      map: map,
+      center: [76.26429,9.9256],
+      zoom: 7, // scale: 72223.819286
       container: "viewDiv",
-      map: webmap
-
+      constraints: {
+        snapToZoom: false
+      }
     });
+
+    //Heart feature layer (points)
+  const HeartLayer = new FeatureLayer({
+    url: "https://services1.arcgis.com/pMeXRvgWClLJZr3s/arcgis/rest/services/geojson_for_my_valentine/FeatureServer/0"
+  });
+
+  map.add(HeartLayer);
+
+//Route feature layer (lines)
+  const RouteLayer = new FeatureLayer({
+    url: "https://services1.arcgis.com/pMeXRvgWClLJZr3s/arcgis/rest/services/geojson_for_my_valentine/FeatureServer/1"
+  });
+
+  map.add(RouteLayer, 0);
+
+
 
 });
